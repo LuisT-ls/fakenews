a0112788cb8f7a69aaee17c89978af530b43f5c0
Luís Teixeira <84951329+LuisT-ls@users.noreply.github.com>
2025-01-30 14:51:38 -0300

    Remoção da duplicação na notificação da limpeza do histórico

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,11 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
       // Mostra notificação de sucesso
       showNotification('Histórico apagado com sucesso!', 'success')
-
-      // Aguarda um tempo para exibir a notificação de feedback
-      setTimeout(() => {
-        showNotification('Obrigado pelo seu feedback!', 'success')
-      }, 2000)
     })
 
   // Listeners de conectividade
